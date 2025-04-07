df0a25c0d500d62574ccb3a9cd20e0aa6d2a5fc6
kurannn <fahrettin.kuran@iusspavia.it>
2025-04-07 12:28:40 +0200

    'docs_add'

--- doc/conf.py
+++ doc/conf.py
@@ -6,9 +6,9 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'QuakeT'
-copyright = 'GEM Foundation and Électricité de France'
-author = 'GEM Foundation and Électricité de France'
+project = 'Example'
+copyright = 'workshop participant'
+author = 'workshop participant'
 release = '0.1'
 
 
@@ -24,5 +24,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
